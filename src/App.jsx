5eab9fa296b5d168801f50ade7c6a623bac4980f
Ahmet Bey <ahmetyusuf18simsek@gmail.com>
2026-05-18 14:07:11 +0300

    ı started learnd

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,34 @@ function MyButton() {
   );
 }
 function Karsilama() {
-  const user = { name: "Ahmet" }; // JavaScript'te verimizi tanımladık
+  const user = { name: "Ahmet" }; 
 
   return (
     <h1> Merhaba {user.name} </h1> 
+  );
+}
+const user = {
+  name: 'Hedy Lamarr',
+  imageUrl: 'https://react.dev/images/docs/scientists/yXOvdOSs.jpg',
+  imageSize: 90,
+};
+// süslü parantez değişken değerler için kullanılır.
+// listlerin içindeki değerlere erişmek için . kullanılır.
+// export diğer dosyalarda da bu fonkisyonları import etmemizi sağlar.
+export function Profile() {
+  return (
+    <>
+      <h1>{user.name}</h1>
+      <img
+        className="avatar"
+        src={user.imageUrl}
+        alt={'Photo of ' + user.name}
+        style={{
+          width: user.imageSize,
+          height: user.imageSize
+        }}
+      />
+    </>
   );
 }
 export default function MyApp() {
@@ -17,6 +41,21 @@ export default function MyApp() {
       <MyButton />
       <Karsilama />
       <img className="avatar" src="https://example.com/avatar.jpg" alt="Avatar" />
+      <Profile></Profile>
+      <Topiclist></Topiclist>
     </div>
   );
 }
+const products = [
+  { title: 'first topic', id: 1 },
+  { title: 'second topic', id: 2 },
+  { title: 'topic x', id: 3 },
+];
+function Topiclist(){
+  return( 
+  <ul>
+    {products.map((prdct) => <li id={prdct.id} className="topicList">{prdct.title}</li>)}
+  </ul>
+  );
+}
+
